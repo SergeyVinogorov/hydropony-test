03b559b569954169fea39882008d242f85056f96
SergeyVinogorov <vinogorich@gmail.com>
2021-02-04 08:32:48 +0300

    set controller

---
+++ src/controller/FilterPageController.ts
@@ -0,0 +1,7 @@
+import { StocksDto } from "../models/StocksDto";
+import { autowired } from "first-di";
+
+export FilterPageController {
+	public stocks: StocksDto[] = []
+	public stocks: StocksDto[] = []
+}
